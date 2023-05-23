e3beb71cac3d8114047b77ceb0c44b89212aa92b
alexis-bruneau <alexis1999bruneau@gmail.com>
2023-05-23 02:47:28 -0400

    modified menu bar js

--- static/js/MenuBar.js
+++ static/js/MenuBar.js
@@ -1,31 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-  fetch('/templates/menu_bar.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('menu').innerHTML = data;
-    })
-    .then(() => {
-      // Function to initialize the menu
-      function initializeMenu() {
-        // Attach an event handler to the document object (or a closer common ancestor if possible)
-        $(document).on('click', 'nav ul li a:not(:only-child)', function(e) {
-          $(this).siblings('.nav-dropdown').toggle();
-          // Close one dropdown when selecting another
-          $('.nav-dropdown').not($(this).siblings()).hide();
-          e.stopPropagation();
-        });
+$(document).ready(function() {
+  $("#menu").load("/menu_bar.html", function() {
+    function initializeMenu() {
+      $(document).on('click', 'nav ul li a:not(:only-child)', function(e) {
+        $(this).siblings('.nav-dropdown').toggle();
+        $('.nav-dropdown').not($(this).siblings()).hide();
+        e.stopPropagation();
+      });
 
-        $(document).click(function() {
-          $('.nav-dropdown').hide();
-        });
+      $(document).click(function() {
+        $('.nav-dropdown').hide();
+      });
 
-        $(document).on('click', '#nav-toggle', function() {
-          $('nav ul').slideToggle();
-          this.classList.toggle('active');
-        });
-      }
+      $(document).on('click', '#nav-toggle', function() {
+        $('nav ul').slideToggle();
+        this.classList.toggle('active');
+      });
+    }
 
-      // Call the function to initialize the menu
-      initializeMenu();
-    });
+    initializeMenu();
+  });
 });
