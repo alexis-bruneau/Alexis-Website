9ac3758127f3d3676bcaf0495c8234093a31cb0c
alexis-bruneau <alexis1999bruneau@gmail.com>
2023-06-10 19:58:56 -0400

    Adding Modified Carousel

--- static/js/CarouselTest.js
+++ static/js/CarouselTest.js
@@ -1,60 +1,61 @@
 $(document).ready(function() {
-  
-  function moveToSelected(element) {
 
-    if (element == "next") {
-      var selected = $(".selected").next();
-    } else if (element == "prev") {
-      var selected = $(".selected").prev();
-    } else {
-      var selected = element;
+    function moveToSelected(element) {
+
+        // Hide the overlay for all images
+        $('#carousel .image-overlay').hide();
+
+        if (element == "next") {
+            var selected = $(".selected").next();
+        } else if (element == "prev") {
+            var selected = $(".selected").prev();
+        } else {
+            var selected = element;
+        }
+  
+        var next = $(selected).next();
+        var prev = $(selected).prev();
+
+        $(selected).removeClass().addClass("selected");
+        $(prev).removeClass().addClass("prev");
+        $(next).removeClass().addClass("next");
+  
+        // Hide all other images
+        $(next).nextAll().removeClass().addClass('hideRight');
+        $(prev).prevAll().removeClass().addClass('hideLeft');
+
+        // Show the overlay for the selected image and update its href
+        var href = $(selected).data('href');  // Get the URL from the data-href attribute
+        $(selected).find('.image-overlay').attr('href', href).show();
     }
 
-    var next = $(selected).next();
-    var prev = $(selected).prev();
-    var prevSecond = $(prev).prev();
-    var nextSecond = $(next).next();
+    $(document).keydown(function(e) {
+        switch(e.which) {
+            case 37: // left
+                moveToSelected('prev');
+                break;
 
-    $(selected).removeClass().addClass("selected");
+            case 39: // right
+                moveToSelected('next');
+                break;
 
-    $(prev).removeClass().addClass("prev");
-    $(next).removeClass().addClass("next");
+            default: return;
+        }
+        e.preventDefault();
+    });
 
-    $(nextSecond).removeClass().addClass("nextRightSecond");
-    $(prevSecond).removeClass().addClass("prevLeftSecond");
+    $('#carousel div').click(function() {
+        moveToSelected($(this));
+    });
 
-    $(nextSecond).nextAll().removeClass().addClass('hideRight');
-    $(prevSecond).prevAll().removeClass().addClass('hideLeft');
+    $('#prev').click(function() {
+        moveToSelected('prev');
+    });
 
-  }
+    $('#next').click(function() {
+        moveToSelected('next');
+    });
 
-  $(document).keydown(function(e) {
-      switch(e.which) {
-          case 37: // left
-          moveToSelected('prev');
-          break;
-
-          case 39: // right
-          moveToSelected('next');
-          break;
-
-          default: return;
-      }
-      e.preventDefault();
-  });
-
-  $('#carousel div').click(function() {
-    moveToSelected($(this));
-  });
-
-  $('#prev').click(function() {
-    moveToSelected('prev');
-  });
-
-  $('#next').click(function() {
-    moveToSelected('next');
-  });
-
-  // Call moveToSelected on the currently selected element when the page loads
-  moveToSelected($(".selected"));
+    // Call moveToSelected on the currently selected element when the page loads
+    moveToSelected($(".selected"));
 });
